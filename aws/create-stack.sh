b4d239958aaac0b4931679d66e08a9eed88e2a34
tkonopka <tokonopka@gmail.com>
2023-08-28 11:05:28 +0100

    buildspec in folder

--- aws/create-stack.sh
+++ aws/create-stack.sh
@@ -7,6 +7,7 @@ aws --profile $AWS_PROFILE \
   --stack-name $STACK_NAME \
   --region $AWS_REGION \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=AwsAccount,ParameterValue=$AWS_ACCOUNT \
+  --parameters  ParameterKey=AwsAccount,ParameterValue=$AWS_ACCOUNT \
+                ParameterKey=StackName,ParameterValue=$STACK_NAME \
   --template-body file://cloudformation.yml
 
